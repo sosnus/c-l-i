e372191502157c539d5cecb34e99f26be6d15ba8
Sosnus <sosnus15@gmail.com>
2022-01-07 03:25:12 +0100

    loop and format this

--- pc/main.py
+++ pc/main.py
@@ -1,20 +1,27 @@
 import subprocess
+import time
+
+commandTab = ["wmic", "cpu", "get", "loadpercentage", "/value"]
 
 def extract(s):
+    # print(s)
     s1 = s[s.find('=')+1:]
-    # print(s1)
-    # print(s1.find("\\"))
     s2 = s1[0:s1.find("\\")]
-    print(s2)
     return s2
 
 
-command = "wmic cpu get loadpercentage /value"
-commandTab = ["wmic", "cpu", "get", "loadpercentage", "/value"]
+# time.sleep(5)
 
-#  subprocess.run(["wmic", "cpu", "get", "loadpercentage"], shell=True)
-# subprocess.run(commandTab, shell=True)
 
-myOut = subprocess.check_output(commandTab, shell=True)
-percent = extract(str(myOut))
-print(percent)
+# myOut = subprocess.check_output(commandTab, shell=True)
+# percent = extract(str(myOut))
+# print(percent)
+
+
+while True:
+  localtime = time.localtime()
+  result = time.strftime("%H:%M:%S", localtime)
+  print(result, end = "  ", flush=True)
+  myOut = subprocess.check_output(commandTab, shell=True)
+  print(extract(str(myOut)+"%"), flush=True)
+  time.sleep(5)
