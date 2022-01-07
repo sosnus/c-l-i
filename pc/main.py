4698f3fdf196c118d6b5f6302163a4fd12a9fdd2
Sosnus <sosnus15@gmail.com>
2022-01-07 17:46:50 +0100

    open serialport

--- pc/main.py
+++ pc/main.py
@@ -1,7 +1,10 @@
 import subprocess
 import time
+import serial
 
 commandTab = ["wmic", "cpu", "get", "loadpercentage", "/value"]
+serialName = "COM6" # '/dev/ttyUSB0'
+ser = serial.Serial(serialName)
 
 def extract(s):
     # print(s)
@@ -24,4 +27,13 @@ while True:
   print(result, end = "  ", flush=True)
   myOut = subprocess.check_output(commandTab, shell=True)
   print(extract(str(myOut)+"%"), flush=True)
+  ser.write(extract(str(myOut)).encode())
   time.sleep(5)
+
+ser.close()
+
+# >>> import serial
+# >>> ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+# >>> print(ser.name)         # check which port was really used
+# >>> ser.write(b'hello')     # write a string
+# >>> ser.close()             # close port
