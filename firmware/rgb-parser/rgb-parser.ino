651c9c196dfb8f491eb83c0e116b1388adff57f8
Sosnus <sosnus15@gmail.com>
2022-01-07 18:46:42 +0100

    try parse it

--- firmware/rgb-parser/rgb-parser.ino
+++ firmware/rgb-parser/rgb-parser.ino
@@ -1,64 +1,65 @@
-//String message = "";         // a String to hold incoming data
 char message[] = {"rgb(126, 127, 128)"};
 bool stringComplete = false;  // whether the string is complete
 char * ptr;
 
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
   Serial.begin(9600);
-//  message.reserve(200);
 }
 
 void loop() {
-
   if (stringComplete) {
     Serial.print("New: ");
     Serial.println(message);
     rgbToPwm();
-    // clear the string:
-    
-//    message = "";
     stringComplete = false;
   }
-  
-
-  // put your main code here, to run repeatedly:
-
 }
 
 void serialEvent() {
   int i = 0;
   while (Serial.available()) {
-    // get the new byte:
     char inChar = (char)Serial.read();
-    // add it to the inputString:
     message[i]= inChar;
-    i++;
-//    message += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
+    ++i;
+    Serial.print(inChar);
+
     if (inChar == '\n') {
       stringComplete = true;
+    Serial.print("New msg in buff: ");
+    Serial.println(message);
     }
   }
 }
 
-void rgbToPwm(){
+void printPtr()
+{
+Serial.print("ptr=");
+Serial.println(ptr);
 
-//          char message[] = {"rgb(126, 127, 128)"};
-//        char * ptr;
+}
+
+void rgbToPwm(){
+         char message[] = {str()};
+       char * ptr;
+printPtr();
         ptr = strtok(message, "("); //move past (
+printPtr();
         ptr = strtok(NULL, ",");  //next comma is red
         byte RED = atoi(ptr);
-        Serial.print("SET LEDS TO....");
+printPtr();
+        Serial.println("SET LEDS TO....");
         Serial.println(RED);
         ptr = strtok(NULL, ",");  //next comma is green
+printPtr();
         byte GREEN = atoi(ptr);
         Serial.println(GREEN);
         ptr = strtok(NULL, ",");  //next comma is blue
         byte BLUE = atoi(ptr);
         Serial.println(BLUE);
-        delay(2000);
+        
+// strncpy(message, "iqbal", sizeof(a) - 1);
+        // for(int i = 0; i >)
+        // delay(2000);
   
 }
