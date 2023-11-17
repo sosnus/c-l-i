ac9edd937d18e1aa48c48f8ed998ec005b167075
Sosnus <sosnus15@gmail.com>
2023-11-17 04:59:45 +0100

    clean!

--- pc/main.py
+++ pc/main.py
@@ -12,22 +12,11 @@ serialName = "COM3" # '/dev/ttyUSB0'
 ser = serial.Serial(serialName)
 
 def extract(s):
-    # print(s)
     s1 = s[s.find('=')+1:]
     s2 = s1[0:s1.find("\\")]
     return s2
 
-
-# time.sleep(5)
-
-
-# myOut = subprocess.check_output(commandTab, shell=True)
-# percent = extract(str(myOut))
-# print(percent)
-
-
 while True:
-# if True:
   localtime = time.localtime()
   result = time.strftime("%H:%M:%S", localtime)
   print(result, end = "  ", flush=True)
@@ -35,12 +24,10 @@ while True:
   print(extract(str(myOut)+"%"), flush=True)
   cpuloadStr = str(myOut)
 
-  # digits = re.findall(r'\d', cpuload)
   digits = re.findall(r'\d+', cpuloadStr)
   print("cpuload")
   cpuload = int(digits[0])
   print(cpuload)
-  # print(digits[0])
 
   if cpuload > 10:
     ser.write(extract(str(cmd_high)).encode())
@@ -49,16 +36,6 @@ while True:
   else:
     ser.write(extract(str(cmd_low)).encode())
 
-
-  # char = "="
-  # cpuload = cpuload[cpuload.find(char)+1:]
-  # char = "\r"
-  # cpuload2 = cpuload.replace(char, "")
-  # # cpuload = cpuload[:cpuload.find(char)+1]
-  # print("cpuload")
-  # print(cpuload)
-  # print(cpuload2)
-  # ser.write(extract(str(myOut)).encode())
   time.sleep(3)
 
 ser.close()
